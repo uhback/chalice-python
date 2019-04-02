37f12ebb08baa06ef99f51fe3f48a4c072c725ac
uhback <gmouse03@gmail.com>
2019-04-03 00:03:17 +1300

    env resetting, add reg_date

--- app.py
+++ app.py
@@ -6,12 +6,18 @@ from chalicelib import db_connect
 
 import logging
 import json
+import datetime
 
 app = Chalice(app_name='toys')
 app.debug = True
              
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+# Convert Date Time to String ("datetime.datetime(2016, 4, 8, 11, 43, 54, 920632)")
+def datetime_converter(o):
+    if isinstance(o, datetime.datetime):
+        return o.__str__()
 
 # GET all users
 @app.route("/users", methods=['GET'])
@@ -22,50 +28,42 @@ def users():
         row_headers = [x[0] for x in cur.description]
         for row in cur:
             result.append(dict(zip(row_headers, row)))
-        print(result)
-    response = {
-        "statusCode": 200,
-        "body": result
-    }
-    return response
+    return json.dumps(result, default=datetime_converter)
+    # return Response(body=result,
+    #                 status_code=200,
+    #                 headers={'Content-Type': 'application/json'})
 
 # GET a user 
 @app.route("/users/{user_id}", methods=['GET'])
 def get_user_info(user_id):
     result = []
     with db_connect.conn.cursor() as cur:
-        cur.execute("SELECT * FROM users WHERE user_id = %s", user_id)
+        cur.execute("SELECT email, user_name, age, gender FROM users WHERE user_id = %s", user_id)
         row_headers = [x[0] for x in cur.description]
         for row in cur:
             result.append(dict(zip(row_headers, row)))
-        print(result)
-    response = {
-        "statusCode": 200,
-        "body": result
-    }
-    return response
+    return json.dumps(result, default=datetime_converter)
+    # return Response(body=result,
+    #                 status_code=200,
+    #                 headers={'Content-Type': 'application/json'})
 
 # CREATE a user
 @app.route("/users", methods=['POST'])
 def create_user():
-    try:
-        user_add = app.current_request.json_body
-        with db_connect.conn.cursor() as cur:
-            sql_query = "INSERT INTO users(email, user_name, age, gender) VALUES('%s', '%s', %d, '%s')" % (user_add['email'], user_add['user_name'], user_add['age'], user_add['gender'])
-            print(sql_query)
-            cur.execute(sql_query)
-            db_connect.conn.commit()
-            cur.close()
-        return json.dumps("Add Success!")
-    except:
-        db_connect.conn.rollback()
+    user_add = app.current_request.json_body
+    with db_connect.conn.cursor() as cur:
+        sql_query = "INSERT INTO users(email, user_name, age, gender) VALUES('%s', '%s', %d, '%s')" % (user_add['email'], user_add['user_name'], user_add['age'], user_add['gender'])
+        cur.execute(sql_query)
+        db_connect.conn.commit()
+        cur.close()
+    return Response(body='Complete',
+                    status_code=200,
+                    headers={'Content-Type': 'application/plain'})
 
 # UPDATE a user
 @app.route("/users/{user_id}", methods=['PUT'])
 def update_user(user_id):
     user_update = app.current_request.json_body
-    print(user_update)
-    print(user_id)
     with db_connect.conn.cursor() as cur:
         for key, value in user_update.items():
             sql_query = "UPDATE users SET %s = '%s' where user_id = %s" % (key, value, user_id)
@@ -73,17 +71,6 @@ def update_user(user_id):
             cur.execute(sql_query)
         db_connect.conn.commit()
         cur.close()
-    return json.dumps("Update Success!")
-
-# UPDATE a user
-# @app.route("/users/{user_id}", methods=['PUT'])
-# def update_user(user_id):
-#     user_update = app.current_request.json_body
-#     with db_connect.conn.cursor() as cur:
-#         sql_query = "UPDATE users SET email='%s', user_name='%s', age='%d', gender='%s' where user_id=%s" % (user_update['email'], user_update['user_name'], user_update['age'], user_update['gender'], user_id)
-#         print(sql_query)
-#         cur.execute(sql_query)
-#         db_connect.conn.commit()
-#         cur.close()
-#     return json.dumps("Update Success!")
-    
+    return Response(body='Complete',
+                    status_code=200,
+                    headers={'Content-Type': 'application/plain'})
